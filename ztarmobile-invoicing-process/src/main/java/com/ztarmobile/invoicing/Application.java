e599c3a317fcd19a7319ad75a891a42e857f903a
Armando Rivas <armandorivasarzaluz@Armandos-iMac.local>
2017-03-27 23:09:12 -0500

    Adding spring configuration

--- ztarmobile-invoicing-process/src/main/java/com/ztarmobile/invoicing/Application.java
+++ ztarmobile-invoicing-process/src/main/java/com/ztarmobile/invoicing/Application.java
@@ -7,7 +7,10 @@
 package com.ztarmobile.invoicing;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Main Application.
@@ -16,6 +19,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 03/27/17
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@ImportResource({ "classpath:com/ztarmobile/invoicing/springframework/applicationContext.xml",
+        "classpath*:com/ztarmobile/invoicing/springframework/**/*-config.xml",
+        "classpath*:com/ztarmobile/invoicing/**/*-dao.xml" })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
