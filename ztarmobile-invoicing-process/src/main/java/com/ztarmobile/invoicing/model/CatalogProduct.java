da972ec6625759868fd361e499ebb8eb0152d11f
Armando Rivas Arzaluz <rivasarmando271084@gmail.com>
2017-04-03 09:42:20 -0500

    Implementing equals and hash method

--- ztarmobile-invoicing-process/src/main/java/com/ztarmobile/invoicing/model/CatalogProduct.java
+++ ztarmobile-invoicing-process/src/main/java/com/ztarmobile/invoicing/model/CatalogProduct.java
@@ -65,6 +65,47 @@ public class CatalogProduct {
     /*
      * (non-Javadoc)
      * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (cdma ? 1231 : 1237);
+        result = prime * result + ((product == null) ? 0 : product.hashCode());
+        result = prime * result + (int) (rowId ^ (rowId >>> 32));
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CatalogProduct other = (CatalogProduct) obj;
+        if (cdma != other.cdma)
+            return false;
+        if (product == null) {
+            if (other.product != null)
+                return false;
+        } else if (!product.equals(other.product))
+            return false;
+        if (rowId != other.rowId)
+            return false;
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see java.lang.Object#toString()
      */
     @Override
