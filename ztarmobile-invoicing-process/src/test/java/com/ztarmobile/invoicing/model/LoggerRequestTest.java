503c68d033dae740c4644cb9a0f1031dabe79f3f
Armando Rivas Arzaluz <rivasarmando271084@gmail.com>
2017-04-20 11:01:08 -0500

    Adding a new testing class.

---
+++ ztarmobile-invoicing-process/src/test/java/com/ztarmobile/invoicing/model/LoggerRequestTest.java
@@ -0,0 +1,37 @@
+/* 
+ * Copyright (C) Ztar Mobile, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Armando Rivas <arivas@ztarmobile.com>, April 2017.
+ */
+package com.ztarmobile.invoicing.model;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Test for {@link com.ztarmobile.invoicing.model.LoggerRequest}
+ * 
+ * @author armandorivas
+ */
+@RunWith(SpringRunner.class)
+public class LoggerRequestTest {
+    /**
+     * Logger for this class
+     */
+    private static final Logger LOG = Logger.getLogger(LoggerRequestTest.class);
+
+    @Test
+    public void setterAndGetters() {
+        LOG.debug("Starting the test for..." + getClass());
+        LoggerRequest loggerRequest = new LoggerRequest();
+        loggerRequest.setRowId(4);
+        Assert.assertEquals(4, loggerRequest.getRowId());
+
+        loggerRequest.setProduct("DATAWIND");
+        Assert.assertEquals("DATAWIND", loggerRequest.getProduct());
+    }
+}
