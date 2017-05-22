94aafbc7e1a7343efc387453d4ffb1e653f0add9
Armando Rivas Arzaluz <rivasarmando271084@gmail.com>
2017-05-22 09:32:39 -0500

    Adding initial project

---
+++ ztarmobile-account-service/src/main/java/com/ztarmobile/account/ZtarAccountManagmentMicroServiceV3Application.java
@@ -0,0 +1,12 @@
+package com.ztarmobile.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtarAccountManagmentMicroServiceV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtarAccountManagmentMicroServiceV3Application.class, args);
+	}
+}
