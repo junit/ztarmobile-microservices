abd39ddc6b0a74e96001278393a62082691b18d7
Armando Rivas Arzaluz <rivasarmando271084@gmail.com>
2017-06-14 14:24:18 -0500

    Adding startup/shutdown scripts

---
+++ ztarmobile-user-profile-service/user-profile/shutdown.sh
@@ -0,0 +1,2 @@
+# Shutdown the service
+curl -X POST localhost:38060/shutdown
---
+++ ztarmobile-user-profile-service/user-profile/startup.sh
@@ -0,0 +1,3 @@
+nohup java -Dspring.datasource.username=xxxxx \
+           -Dspring.datasource.password=xxxxx \
+           -jar ztarmobile-user-profile-service-3.0.0.RELEASE.jar > log/ztarmobile-user-profile-service.log &
